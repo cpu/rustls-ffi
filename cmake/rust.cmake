8fe92df471bda65c1c8d9573eb7cd30d61709aa2
Daniel McCarney <daniel@binaryparadox.net>
2024-11-27 13:35:17 -0500

    wip: tmp cmake

--- cmake/rust.cmake
+++ cmake/rust.cmake
@@ -5,12 +5,13 @@ ExternalProject_Add(
     rustls-ffi
     DOWNLOAD_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND
-        cargo capi build --locked ${CARGO_FEATURES}
-        "$<IF:$<CONFIG:Release>,--release,-->"
+    BUILD_COMMAND ""
     # Rely on cargo checking timestamps, rather than tell CMake where every
     # output is.
     BUILD_ALWAYS true
+    COMMAND
+        cargo capi build --locked ${CARGO_FEATURES}
+        "$<IF:$<CONFIG:Release>,--release,-->"
     INSTALL_COMMAND
         cargo capi install --libdir=lib --prefix=${CMAKE_BINARY_DIR}/rust
         --locked ${CARGO_FEATURES} "$<IF:$<CONFIG:Release>,--release,-->"
@@ -18,25 +19,9 @@ ExternalProject_Add(
     # of "error" in stdout as an error, and we have some test functions that
     # end in "_error". Quiet mode suppresses test names, so this is a
     # sufficient workaround.
-    TEST_COMMAND
-        cargo test --locked ${CARGO_FEATURES}
-        "$<IF:$<CONFIG:Release>,--release,-->" --quiet
-)
-
-add_custom_target(
-    cbindgen
-    # TODO(@cpu): I suspect this won't work on Windows :P
-    COMMAND cbindgen > "src/rustls.h"
-    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
-)
-
-add_custom_target(
-    connect-test
-    DEPENDS client
-    COMMAND
-        ${CMAKE_COMMAND} -E env RUSTLS_PLATFORM_VERIFIER=1
-        ${CMAKE_BINARY_DIR}/tests/client example.com 443 /
-    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+    #TEST_COMMAND
+    #    cargo test --locked ${CARGO_FEATURES}
+    #   "$<IF:$<CONFIG:Release>,--release,-->" --quiet
 )
 
 if(WIN32)
@@ -48,6 +33,33 @@ else()
 endif()
 
 add_custom_target(
+    connect-test
+    DEPENDS client
+    COMMAND
+        ${CMAKE_COMMAND} -E env RUSTLS_PLATFORM_VERIFIER=1 ${CLIENT_BINARY}
+        example.com 443 /
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+# TODO(@cpu): write this as a special case client_server.rs test.
+add_custom_target(
+    ech-test
+    DEPENDS client
+    # Fetch ECH configs from DNS using the Rust test utility.
+    COMMAND
+        cargo test --test ech_fetch -- research.cloudflare.com
+        /tmp/research.cloudflare.com.ech.configs.der
+    # Run the client example with the ECH configs.
+    COMMAND
+        ${CMAKE_COMMAND} -E env
+        SSLKEYLOGFILE=/tmp/ech_test.keys # TODO(@cpu): remove.
+        ${CMAKE_COMMAND} -E env RUSTLS_PLATFORM_VERIFIER=1 ${CMAKE_COMMAND} -E
+        env RUSTLS_ECH_CONFIG_LIST=/tmp/research.cloudflare.com.ech.configs.der
+        ${CLIENT_BINARY} research.cloudflare.com 443 /cdn-cgi/trace
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+add_custom_target(
     integration-test
     DEPENDS client server
     COMMAND
@@ -55,5 +67,12 @@ add_custom_target(
         -E env SERVER_BINARY=${SERVER_BINARY} cargo test --locked
         ${CARGO_FEATURES} "$<IF:$<CONFIG:Release>,--release,>" --test
         client_server client_server_integration -- --ignored --exact
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+add_custom_target(
+    cbindgen
+    # TODO(@cpu): I suspect this won't work on Windows :P
+    COMMAND cbindgen > "src/rustls.h"
     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
 )
